f9069a7016a30cdadd0c89f8879ed84809deb674
Rick Lim <rick.jason.a.lim@gmail.com>
2019-10-21 21:29:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -21,6 +21,7 @@ def introduction_with_language_optional(Rick, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
+introduction_with_language_optional
 
 
 
